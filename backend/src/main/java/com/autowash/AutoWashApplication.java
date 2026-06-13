0dd88a2a188a1bba4d124a5af84cf2128d00d684
Tuan Kit <Kit@gmail.com>
2026-06-13 15:49:02 +0700

    update

--- backend/src/main/java/com/autowash/AutoWashApplication.java
+++ backend/src/main/java/com/autowash/AutoWashApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoWashApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AutoWashApplication.class, args);
     }
 
